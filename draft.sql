30e73ac6982923b51f87e652907370424c7e7086
Deepan Maitra <2019csb1044@iitrpr.ac.in>
2021-10-20 19:55:53 +0530

    Create draft.sql

---
+++ draft.sql
@@ -0,0 +1,2 @@
+INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
+VALUES(DEFAULT, 'CY101', 'Chemistry for Engineers', 'CY', 1, 1, 2019, 3, 2, 1, 1, 3);
