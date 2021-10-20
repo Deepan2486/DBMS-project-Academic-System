c667673fd79b2990619a5795f15af4efc1d9042f
Deepan Maitra <2019csb1044@iitrpr.ac.in>
2021-10-20 22:41:43 +0530

    Update draft.sql

--- draft.sql
+++ draft.sql
@@ -1,19 +1,15 @@
-CREATE TABLE Course_Offering(
+CREATE TABLE Course_Catalogue(
 	
-	index SERIAL UNIQUE,
 	course_id varchar(100) UNIQUE,
 	course_name varchar(300) UNIQUE,
 	department varchar(50),
-	section INT,
-	semester INT,
-	year INT,
 	L INT,
 	T INT,
 	P INT,
 	S INT,
 	C INT,
 	
-	PRIMARY KEY (course_id, section, semester, year)
+	PRIMARY KEY (course_id)
 );
 
 CREATE TABLE Instructor(
@@ -27,13 +23,16 @@ CREATE TABLE Instructor(
 	PRIMARY KEY (ins_id, ins_name, department)
 );
 
-CREATE TABLE teaches(
+CREATE TABLE course_offering(
 	
-	course_id varchar(100),
+	course_id varchar(100) UNIQUE,
 	ins_id INT,
 	timetable_slot varchar(50),
+	section INT,
+	year INT,
+	semester INT,
 	
-	PRIMARY KEY (timetable_slot),
+	PRIMARY KEY (course_id, section, year, semester),
 	
 	CONSTRAINT fk_courseid FOREIGN KEY(course_id) REFERENCES course_offering(course_id),
 	CONSTRAINT fk_insid FOREIGN KEY(ins_id) REFERENCES instructor(ins_id)
@@ -41,15 +40,47 @@ CREATE TABLE teaches(
 );
 
 --Filling course offering table
-INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
-VALUES(DEFAULT, 'CY101', 'Chemistry for Engineers', 'CY', 1, 1, 2019, 3, 2, 1, 1, 3);
-INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
-VALUES(DEFAULT, 'GE103', 'Introduction to Programming', 'CS', 1, 1, 2019, 3, 2, 1, 1, 4.5);
-INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
-VALUES(DEFAULT, 'EE101', 'Introduction to Electrical Engineering', 'EE', 1, 2, 2019, 3, 2, 1, 1, 4);
-INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
-VALUES(DEFAULT, 'CS201', 'Data structures', 'CS', 1, 1, 2020, 3, 2, 1, 1, 4);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('CY101', 'Chemistry for Engineers', 'CY', 3, 2, 1, 1, 3);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES( 'GE103', 'Introduction to Programming', 'CS', 3, 2, 1, 1, 4.5);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('EE101', 'Introduction to Electrical Engineering', 'EE', 3, 2, 1, 1, 4);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('CS201', 'Data structures', 'CS', 3, 2, 1, 1, 4);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('HS202', 'HUMAN GEOGRAPHY', 'HS', 3, 2, 1, 1, 3); 
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('CE101', 'INTRO TO CIVIL ENGINEERING', 'CE', 3, 2, 1, 1, 3);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('ME101', 'Mechanics', 'ME', 3, 2, 1, 1, 3);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('GE111', 'EVS', 'GE', 3, 2, 1, 1, 3);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('MA102', 'Linear Algebra', 'MA', 3, 2, 1, 1, 3);
+INSERT INTO course_catalogue(course_id, course_name, department, l, t, p, s, c)
+VALUES('CH101', 'Introduction to Chemical Engineering', 'CH', 3, 2, 1, 1, 3);
 
 --Filling Instructor table
 INSERT INTO instructor(ins_id, ins_name, department, age, position)
 VALUES (DEFAULT, 'Amit Sharma', 'EE', 26, 'Assistant professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Karishma Roy', 'EE', 49, 'Associate Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Parakram Suchi', 'CE', 45, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Saryu Sengupta', 'MNC', 38, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Kartik Matta', 'ME', 32, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Swati Sharma', 'CH', 52, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Ekta Basu', 'CS', 47, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Virat Bindra', 'ME', 29, 'Assistant Professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Rohit Kumar', 'CS', 36, 'Associate professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Subham Singh', 'CH', 45, 'Associate professor');
+INSERT INTO instructor(ins_id, ins_name, department, age, position)
+VALUES (DEFAULT, 'Rahul Sabharwal', 'CE', 32, 'Associate professor');
