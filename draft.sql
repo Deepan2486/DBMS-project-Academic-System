d719c477bc1ac8fd0928978de39562102bd55647
pandeyshivam13 <73707347+pandeyshivam13@users.noreply.github.com>
2021-10-20 20:01:14 +0530

    Update draft.sql

--- draft.sql
+++ draft.sql
@@ -1,2 +1,3 @@
 INSERT INTO course_offering(index, course_id, course_name, department, section, semester, year, l, t, p, s, c)
 VALUES(DEFAULT, 'CY101', 'Chemistry for Engineers', 'CY', 1, 1, 2019, 3, 2, 1, 1, 3);
+VALUES(DEFAULT, 'GE103', 'Introduction to Programming', 'CS', 1, 1, 2019, 3, 2, 1, 1, 4.5);
